edb46c9dd984ac285dc1ae4f29eaaa86d90d13a0
Muhammad Hamza <mhuzsifa684@gmail.com>
2026-03-01 11:22:54 +0500

    define reconstruction path function

--- algorithm.py
+++ algorithm.py
@@ -3,10 +3,22 @@ import math
 from grid import EMPTY, WALL, START, GOAL, VISITED, FRONTIER, PATH
 
 
-# ─── Heuristics ───────────────────────────────────────────────────────────────
+# ─── Heuristics
 
 def manhattan(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 def euclidean(a, b):
     return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+
+# ─── Reconstruct path from came_from dict
+
+def reconstruct_path(came_from, start, goal):
+    path = []
+    node = goal
+    while node != start:
+        path.append(node)
+        node = came_from[node]
+    path.append(start)
+    path.reverse()
+    return path
